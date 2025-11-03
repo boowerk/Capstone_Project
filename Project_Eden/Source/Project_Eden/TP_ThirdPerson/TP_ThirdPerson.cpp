7d08ddba69e70dac892af1f791c09bf7779d862f
Lim <bigmanmin1@naver.com>
2025-11-03 14:28:43 +0900

    fix: TP_ThirdPerson header priority to 0

--- Project_Eden/Source/Project_Eden/TP_ThirdPerson/TP_ThirdPerson.cpp
+++ Project_Eden/Source/Project_Eden/TP_ThirdPerson/TP_ThirdPerson.cpp
@@ -1,7 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#include "Project_Eden.h"
 #include "TP_ThirdPerson.h"
+#include "Project_Eden.h"
 #include "Modules/ModuleManager.h"
 
 //IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TP_ThirdPerson, "TP_ThirdPerson" );
