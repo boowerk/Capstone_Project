d0f982614a4350d2fa155a9ad4595f5d0d7d5af3
Lim <bigmanmin1@naver.com>
2025-11-19 16:13:00 +0900

    remove: TP_ThirdPerson

--- Project_Eden/Source/Project_Eden/TP_ThirdPerson/TP_ThirdPerson.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TP_ThirdPerson.h"
-#include "Project_Eden.h"
-#include "Modules/ModuleManager.h"
-
-//IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TP_ThirdPerson, "TP_ThirdPerson" );
-
-DEFINE_LOG_CATEGORY(LogTP_ThirdPerson)
