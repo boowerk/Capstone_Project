7c225e2397c939e0007af9647c562db25236afca
Lim <bigmanmin1@naver.com>
2025-11-19 16:13:00 +0900

    remove: TP_ThirdPerson

--- Project_Eden/Source/Project_Eden/Project_Eden.Build.cs
+++ Project_Eden/Source/Project_Eden/Project_Eden.Build.cs
@@ -17,39 +17,10 @@ public class Project_Eden : ModuleRules
 			"Json",
 			"JsonUtilities",
             "PCG",
-			"AIModule",
-            "StateTreeModule",
-            "GameplayStateTreeModule",
-            "UMG",
-            "Slate"
+			
         });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {});
-
-        PublicIncludePaths.AddRange(new string[] {
-            ModuleDirectory,
-            "Project_Eden/TP_ThirdPerson",
-            "Project_Eden/TP_ThirdPerson/Variant_Platforming",
-            "Project_Eden/TP_ThirdPerson/Variant_Platforming/Animation",
-            "Project_Eden/TP_ThirdPerson/Variant_Combat",
-            "Project_Eden/TP_ThirdPerson/Variant_Combat/AI",
-            "Project_Eden/TP_ThirdPerson/Variant_Combat/Animation",
-            "Project_Eden/TP_ThirdPerson/Variant_Combat/Gameplay",
-            "Project_Eden/TP_ThirdPerson/Variant_Combat/Interfaces",
-            "Project_Eden/TP_ThirdPerson/Variant_Combat/UI",
-            "Project_Eden/TP_ThirdPerson/Variant_SideScrolling",
-            "Project_Eden/TP_ThirdPerson/Variant_SideScrolling/AI",
-            "Project_Eden/TP_ThirdPerson/Variant_SideScrolling/Gameplay",
-            "Project_Eden/TP_ThirdPerson/Variant_SideScrolling/Interfaces",
-            "Project_Eden/TP_ThirdPerson/Variant_SideScrolling/UI"
-        });
-
-        // Uncomment if you are using Slate UI
-        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
-        // Uncomment if you are using online features
-        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+        
     }
 }
