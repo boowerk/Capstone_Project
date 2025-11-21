ada50b1fb3ad2751a7e122d02243a65094f01c27
Lim <bigmanmin1@naver.com>
2025-11-21 17:13:46 +0900

    feat: add GP_Tags (namespace: GPTags)

---
+++ Project_Eden/Source/Project_Eden/Public/GameplayTags/GP_Tags.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+// 파일명만 일관성을 유지하기위해 _를 사용함 - 슝민
+namespace GPTags
+{
+	namespace GPAbilities
+	{
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(Primary);
+	}
+}
+
