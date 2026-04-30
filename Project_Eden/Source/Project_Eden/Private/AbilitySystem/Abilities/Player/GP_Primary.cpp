59931b64b2aa09ce0011ed1de4dbc5f50ab75f24
Lim <bigmanmin1@naver.com>
2026-04-30 16:49:20 +0900

    fix: set current input direction

--- Project_Eden/Source/Project_Eden/Private/AbilitySystem/Abilities/Player/GP_Primary.cpp
+++ Project_Eden/Source/Project_Eden/Private/AbilitySystem/Abilities/Player/GP_Primary.cpp
@@ -6,6 +6,7 @@
 #include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
 #include "Abilities/Tasks/AbilityTask_WaitInputPress.h"
 #include "Abilities/Tasks/AbilityTask_WaitGameplayEvent.h"
+#include "Player/GP_PlayerController.h"
 #include "Utils/GP_BlueprintLibrary.h"
 
 UGP_Primary::UGP_Primary()
@@ -64,6 +65,25 @@ void UGP_Primary::StartComboSequence()
 	{
 		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
 		return;
+	}
+	
+	if (AGP_PlayerController* PCtrl = Cast<AGP_PlayerController>(PC->GetController()))
+	{
+		FVector2D MoveInput = PCtrl->GetCurrentMoveInput();
+		if (!MoveInput.IsNearlyZero())
+		{
+			const FRotator YawRotation(0.f, PCtrl->GetControlRotation().Yaw, 0.f);
+			const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+			const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+			// 2D 입력 벡터를 3D 월드 방향 벡터로 변환
+			FVector DesiredDirection = (ForwardDirection * MoveInput.Y) + (RightDirection * MoveInput.X);
+			DesiredDirection.Z = 0.0f; 
+			DesiredDirection.Normalize();
+
+			// 캐릭터의 방향을 즉시 변경
+			PC->SetActorRotation(DesiredDirection.Rotation());
+		}
 	}
 
 	ClearExistingTasks();
