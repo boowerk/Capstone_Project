c11579b3387cf701121601e9c76e12d358beeb8e
douyun0623 <dyk6610@naver.com>
2026-05-12 10:53:36 +0900

    feat: launch dedicated server test client in windowed mode

--- Project_Eden/Scripts/DedicatedServer/StartEditorClient_Localhost.bat
+++ Project_Eden/Scripts/DedicatedServer/StartEditorClient_Localhost.bat
@@ -3,6 +3,8 @@ setlocal
 
 set "PROJECT_ROOT=%~dp0..\..\"
 set "UPROJECT=%PROJECT_ROOT%Project_Eden.uproject"
+set "CLIENT_URL=127.0.0.1:7778"
+set "CLIENT_WINDOW_ARGS=-windowed -ResX=960 -ResY=540 -WinX=60 -WinY=60"
 
 if "%UE_SERVER_ROOT%"=="" set "UE_SERVER_ROOT=C:\Engine_server\Windows"
 set "EDITOR_EXE=%UE_SERVER_ROOT%\Engine\Binaries\Win64\UnrealEditor.exe"
@@ -14,5 +16,5 @@ if not exist "%EDITOR_EXE%" (
     exit /b 1
 )
 
-"%EDITOR_EXE%" "%UPROJECT%" 127.0.0.1:7778 -game -log
+"%EDITOR_EXE%" "%UPROJECT%" %CLIENT_URL% -game -log %CLIENT_WINDOW_ARGS% %*
 pause
