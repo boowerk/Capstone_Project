9b0e6c661312fbbc30446e662c016d9715c78cbf
boowerk <back20044@gmail.com>
2026-06-07 21:59:43 +0900

    feat(pcg): notify minimap after layout generation

--- Project_Eden/Source/Project_Eden/Private/PCG/PcgControllerActor.cpp
+++ Project_Eden/Source/Project_Eden/Private/PCG/PcgControllerActor.cpp
@@ -1,32 +1,48 @@
-﻿#include "PCG/PcgControllerActor.h"
+#include "PCG/PcgControllerActor.h"
+
 #include "AI/OpenAIRequester.h"
 #include "Components/SceneComponent.h"
+#include "UI/GP_MinimapSubsystem.h"
 
-
-// Sets default values
 APcgControllerActor::APcgControllerActor()
 {
-    PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = false;
 
-    RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
-
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
 	OpenAIRequester = CreateDefaultSubobject<UOpenAIRequester>(TEXT("OpenAIRequester"));
 }
 
-// Called when the game starts or when spawned
 void APcgControllerActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
-    if (OpenAIRequester)
-    {
-        OpenAIRequester->OnScatterParams.AddDynamic(this, &APcgControllerActor::HandleScatterParams);
 
-        OpenAIRequester->SendOpenAIRequest();
-    }
+	if (OpenAIRequester)
+	{
+		OpenAIRequester->OnScatterParams.AddDynamic(this, &APcgControllerActor::HandleScatterParams);
+		OpenAIRequester->SendOpenAIRequest();
+	}
 }
 
 void APcgControllerActor::HandleScatterParams(FPCGScatterParams Params)
 {
-    ApplyPcgParameters(Params);
+	ApplyPcgParameters(Params);
+
+	if (bAutoNotifyMinimapAfterApplyParameters)
+	{
+		// BP에서 적용한 PCG 결과가 월드에 반영될 시간을 주고 현재 배치를 미니맵에 다시 캡처합니다.
+		NotifyPcgGenerationFinished(AutoMinimapCaptureDelay);
+	}
+}
+
+void APcgControllerActor::NotifyPcgGenerationFinished(float MinimapCaptureDelay)
+{
+	OnPcgLayoutReady.Broadcast(this);
+
+	if (UWorld* World = GetWorld())
+	{
+		if (UGP_MinimapSubsystem* MinimapSubsystem = World->GetSubsystem<UGP_MinimapSubsystem>())
+		{
+			MinimapSubsystem->NotifyPcgLayoutReady(MinimapCaptureDelay);
+		}
+	}
 }
