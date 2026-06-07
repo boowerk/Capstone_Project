1dda073cc86259ff471a372601aa412983d73671
boowerk <back20044@gmail.com>
2026-06-07 21:59:10 +0900

    feat(ui): add dynamic minimap capture

---
+++ Project_Eden/Source/Project_Eden/Public/UI/GP_MinimapSubsystem.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "GP_MinimapSubsystem.generated.h"
+
+class AGP_MinimapCaptureActor;
+class UTextureRenderTarget2D;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FGPMinimapRenderTargetChanged, UTextureRenderTarget2D*, RenderTarget);
+
+UCLASS()
+class PROJECT_EDEN_API UGP_MinimapSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Deinitialize() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Minimap")
+	void RegisterCaptureActor(AGP_MinimapCaptureActor* CaptureActor);
+
+	UFUNCTION(BlueprintCallable, Category = "Minimap")
+	void CaptureFullMap(AActor* BoundsActor = nullptr);
+
+	UFUNCTION(BlueprintCallable, Category = "Minimap")
+	void NotifyPcgLayoutReady(float CaptureDelay = 0.25f);
+
+	UFUNCTION(BlueprintPure, Category = "Minimap")
+	AGP_MinimapCaptureActor* GetActiveCaptureActor();
+
+	UFUNCTION(BlueprintPure, Category = "Minimap")
+	UTextureRenderTarget2D* GetMinimapRenderTarget();
+
+	UPROPERTY(BlueprintAssignable, Category = "Minimap")
+	FGPMinimapRenderTargetChanged OnRenderTargetChanged;
+
+private:
+	AGP_MinimapCaptureActor* ResolveCaptureActor();
+	void BroadcastCurrentRenderTarget();
+
+	TWeakObjectPtr<AGP_MinimapCaptureActor> ActiveCaptureActor;
+	FTimerHandle DelayedCaptureTimerHandle;
+};
