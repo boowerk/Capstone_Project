54878b0797679ae94f1f06df9c4a7ec3f2a10601
boowerk <back20044@gmail.com>
2026-07-11 19:14:19 +0900

    feat(region-events): visualize test triggers

--- Project_Eden/Source/Project_Eden/Public/Game/RegionEvents/GP_RegionEventTestTriggerActor.h
+++ Project_Eden/Source/Project_Eden/Public/Game/RegionEvents/GP_RegionEventTestTriggerActor.h
@@ -7,7 +7,10 @@
 class AGP_PlayerCharacter;
 class AGP_RegionEventActor;
 class UGP_RegionEventData;
+class UPointLightComponent;
 class USphereComponent;
+class UStaticMeshComponent;
+class UTextRenderComponent;
 
 /**
  * Editor/PIE smoke-test helper that starts one Region Event without requiring GameMode zone flow.
@@ -21,6 +24,8 @@ class PROJECT_EDEN_API AGP_RegionEventTestTriggerActor : public AActor
 public:
 	AGP_RegionEventTestTriggerActor();
 
+	virtual void OnConstruction(const FTransform& Transform) override;
+
 	UFUNCTION(BlueprintCallable, Category = "Region Event|Test")
 	AGP_RegionEventActor* TriggerRegionEvent();
 
@@ -30,6 +35,21 @@ public:
 	UFUNCTION(BlueprintPure, Category = "Region Event|Test")
 	AGP_RegionEventActor* GetActiveRegionEvent() const { return ActiveEvent; }
 
+	UFUNCTION(BlueprintPure, Category = "Region Event|Test")
+	UGP_RegionEventData* GetEventData() const { return EventData; }
+
+	UFUNCTION(BlueprintPure, Category = "Region Event|Test")
+	int32 GetTestRegionId() const { return TestRegionId; }
+
+	UFUNCTION(BlueprintPure, Category = "Region Event|Test")
+	bool IsTriggerOnBeginPlay() const { return bTriggerOnBeginPlay; }
+
+	UFUNCTION(BlueprintPure, Category = "Region Event|Test")
+	bool IsTriggerOnPlayerOverlap() const { return bTriggerOnPlayerOverlap; }
+
+	UFUNCTION(BlueprintPure, Category = "Region Event|Test")
+	FText GetStationTitle() const { return StationTitle; }
+
 protected:
 	virtual void BeginPlay() override;
 
@@ -38,6 +58,15 @@ protected:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Region Event|Test")
 	TObjectPtr<USphereComponent> TriggerSphere;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Region Event|Test|Presentation")
+	TObjectPtr<UStaticMeshComponent> VisualPlatform;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Region Event|Test|Presentation")
+	TObjectPtr<UTextRenderComponent> StationLabel;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Region Event|Test|Presentation")
+	TObjectPtr<UPointLightComponent> StationLight;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Region Event|Test")
 	TObjectPtr<UGP_RegionEventData> EventData;
@@ -63,6 +92,15 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Region Event|Test", meta = (Units = "cm"))
 	FVector EventSpawnOffset = FVector(0.0f, 0.0f, 60.0f);
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Region Event|Test|Presentation")
+	FText StationTitle = FText::FromString(TEXT("REGION EVENT TEST"));
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Region Event|Test|Presentation")
+	FLinearColor StationColor = FLinearColor(1.0f, 0.35f, 0.05f, 1.0f);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Region Event|Test|Presentation")
+	bool bShowStationVisuals = true;
+
 private:
 	UFUNCTION()
 	void HandleTriggerSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
@@ -72,6 +110,7 @@ private:
 	void HandleSpawnedEventStateChanged(AGP_RegionEventActor* EventActor);
 
 	void TriggerRegionEventNextTick();
+	void RefreshStationPresentation();
 
 private:
 	UPROPERTY(Transient)
